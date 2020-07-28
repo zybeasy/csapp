6dba98016fa04fa22eac1938de02994d1e79863c
zybeasy <feihong@FeiHong.localdomain>
2020-07-29 01:09:51 +0800

    chapter03 init

---
+++ chapter03/mstore_cpp.s
@@ -0,0 +1,38 @@
+	.file	"mstore.c"
+	.text
+	.globl	_Z9multstorellPl
+	.type	_Z9multstorellPl, @function
+_Z9multstorellPl:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	movq	%rdx, %rbx
+	call	_Z5mult2ll@PLT
+	movq	%rax, (%rbx)
+	popq	%rbx
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	_Z9multstorellPl, .-_Z9multstorellPl
+	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
